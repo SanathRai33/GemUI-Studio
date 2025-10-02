188a9af629fa3215e80a015b1f24e5226de4bf95
SanathRai <sanathrai03@gmail.com>
2025-10-02 23:21:13 +0530

    Chat bot UI designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGem } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="parent-container">
+      <div className="preview-container">
+        <div className="empty-container">
+          <p>Describe your react component in input box and click on generate</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="input-container">
+        <textarea
+          className="preview-textarea"
+          placeholder="Describe your react component"
+        />
+        <button className="generate-button">
+          <span>Generate</span> 
+          <FontAwesomeIcon icon={faGem} />
 
-export default App
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
